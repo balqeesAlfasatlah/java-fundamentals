030a61ad3207620c1757b892d38c1db43f43088b
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-10-13 07:43:46 +0300

    done

---
+++ linter/app/src/test/resources/manyErrorsFile.js
@@ -0,0 +1,14 @@
+zf = or(or(and(a,b), and(b,z)), and(a,z));
+
+  j = or(a, nor(b,z));
+  k = or(b, nor(a,z))
+  l = or(z, nor(a,b))
+
+  console.log("jkl", j, k, l)
+  ones = xor(j, xor(k, l));
+  return {ones, zf};
+console.log("nor 00", nor(0,0));
+console.log("nor 01", nor(0,1));
+console.log("nor 10", nor(1,0))
+console.log("nor 11", nor(1,1))
+console.log();
